dc4f3bfd297b960336393b27bb431fbd22d963e4
jonny <jonnyfsunday22@gmail.com>
2025-11-04 16:35:55 -0600

    Fix CORS for Netlify origin

--- backend/settings.py
+++ backend/settings.py
@@ -132,6 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'https://grok-fullstack-app.netlify.app',
+    'http://localhost:4200',
 ]
 
 CORS_ALLOW_HEADERS = ['*']
