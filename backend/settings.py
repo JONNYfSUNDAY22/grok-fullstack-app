0b2c0b0599b64a53bf06817c40b722cb6f6e0212
jonny <jonnyfsunday22@gmail.com>
2025-11-04 16:11:16 -0600

    Fix CORS for Netlify origin

--- backend/settings.py
+++ backend/settings.py
@@ -130,7 +130,12 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://grok-fullstack-app.netlify.app',
+]
+
+CORS_ALLOW_HEADERS = ['*']
+CORS_ALLOW_METHODS = ['*']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
